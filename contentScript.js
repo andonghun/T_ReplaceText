589dc334d44dca2f72eecb447a534939f79247ac
donghunan <iandonghun@gmail.com>
2020-12-31 13:57:37 +0900

    Make basic features of App

---
+++ contentScript.js
@@ -0,0 +1,13 @@
+function replaceText(find, replace) {
+  const regex = new RegExp(find, "gi");
+  const contents = document.querySelectorAll("p,li,span,h1,h2,h3,h4,h5,h6");
+  for (const snippet of contents) {
+    snippet.textContent = snippet.textContent.replace(regex, replace);
+  }
+}
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.action === "REPLACE_TEXT") {
+    replaceText(message.find, message.replace);
+  }
+});
